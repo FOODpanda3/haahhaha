62b5d772a07872a10c6c83be8daaed69119b6c65
FOODpanda <jamesanthonyvillacorta1@gmail.com>
2025-06-07 18:00:29 +0800

    Add files via upload

---
+++ cj/src/cj/DBconnection.java
@@ -0,0 +1,25 @@
+package cj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/student_management_db";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "james081505";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL JDBC Driver not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Connection failed! Check output console.");
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
